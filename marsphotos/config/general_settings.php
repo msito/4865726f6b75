411eb7902f68fd42704b68d3563a03ef83d6f922
Shino <github_msito@pinksoft.net>
2016-05-02 15:07:42 -0400

    modified mars photos to use different otken ID for different settings

---
+++ marsphotos/config/general_settings.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ */
+
+$rovers = array('curiosity', 'opportunity', 'spirit');
+
+/**
+ * Directories
+ */
+
+$dir_private = '/private';
+$dir_controller = '/controller';
+$dir_model = '/model';
+
---
+++ marsphotos/config/token.php
@@ -0,0 +1,6 @@
+<?php
+if( $_SERVER['SERVER_NAME'] === 'localhost' || $_SERVER['SERVER_NAME'] === '127.0.0.1' ){
+    $api_key = 'DEMO_KEY';
+} else {
+    $api_key = 'LeATHlsoxSJo6JGxQrNQwpA71ANMDATfLHoF0wjB';
+}
